acee41c5c4bc056ba25fb3615083aa4f2c0c60eb
thiagocessin <thiagoacessin@gmail.com>
2021-10-19 16:16:53 -0300

    Ajuste delay do sensor de umidade

--- regador_sensor_umidade.ino
+++ regador_sensor_umidade.ino
@@ -10,7 +10,7 @@ int ValAnalogIn; // Introduz o valor analógico ao código
  
 void setup() {
  // inicio da comunicação serial
-  Serial.begin(9600);
+  Serial.begin(57600);
   servo.attach(SERVO);
   servo.write(90);
 }
@@ -19,21 +19,20 @@ void loop() {
 // Leitura do sensor no pino A0 e armazenamento em SensorValue
   int sensorValue = analogRead(A0);
   int porcento = map(sensorValue, 1023, 0, 0, 100); // Relaciona o valor analógico à porcentagem
-  
    if(porcento < 40) {
-       Serial.println("{\"info\":\"terra seca, regando planta\"}");
+       Serial.print("{\"deviceUUID\":\"b146b053-643a-4640-8a37-b34b888b0dc5\",\"info\":\"terra seca, regando planta\", \"porcentagem\":");
        Serial.print(porcento);
-       Serial.println("%");
-       //delay(300);
+       Serial.println("}");
        servo.write(10);
-       delay(5000);
+       //se a terra estiver seca, permanece 5 segundos na rega e verifica novamente
+       delay(5000);       
     }else{
-      Serial.println("{\"info\":\"umidade normal\"}");
-      Serial.print(porcento);
-      Serial.println("%");
-      
-      servo.write(90);
-   }
-  delay(3000);
-
+       Serial.print("{\"deviceUUID\":\"b146b053-643a-4640-8a37-b34b888b0dc5\",\"info\":\"umidade normal\", \"porcentagem\":");
+       Serial.print(porcento);
+       Serial.println("}");
+       servo.write(90);
+       //se a terra estiver com a umidade OK, aguarda 10 segundos para verificar novamente
+       delay(10000);
+     }
+  
 }
